67e488fe5ca253a08a6ab1a617acbd9dcb6ec2a7
vortexrap <nbnova12@gmail.com>
2019-07-02 17:00:28 -0400

    Update user_data.sh
    Removing Sudo references.

--- modules/web/files/user_data.sh
+++ modules/web/files/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script initializes Amazon Linux 2 AMI and installs NGINX
 
-sudo yum update -y
-sudo amazon-linux-extras install nginx1.12 -y
-sudo systemctl start nginx
+yum update -y
+amazon-linux-extras install nginx1.12 -y
+systemctl start nginx
